1a5f355f64ac565ddaacbed947a57af0b5a7566d
Peter Simons <peter.simons@solarwinds.com>
2018-07-14 12:51:42 +0100

    First Extension and project creation

---
+++ IAppExtensions/IAppExtensions/Extensions/ExtensionPoperties.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Xamarin.UITest
+{
+    public static class ExtensionPoperties
+    {
+        public static int DEFAULT_TIMEOUT_SECONDS = 30;
+        public static double DEFAULT_RETRYFREQUENCY_SECONDS = 0.5;
+        public static double DEFAULT_POST_TIMEOUT_SECONDS = 0.5;
+    }
+}
